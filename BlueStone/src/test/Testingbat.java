9da0d2c2620fd6c1a6c0bbd6e5c0ad5361d0d8ba
mgadigii <mgadigii@yahoo.co.in>
2019-04-01 17:02:36 +0530

    commit

---
+++ BlueStone/src/test/Testingbat.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Testingbat {
+	
+	@Test
+	public void testing_jenkins()
+	{
+		System.out.println("***************Welccome****************");
+		System.out.println("Tesing Jenkins");
+		System.out.println("***************Bye****************");
+	}
+
+}
